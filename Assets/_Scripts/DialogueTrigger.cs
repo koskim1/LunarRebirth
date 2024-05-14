ae6dc61819e6a38ce5a22a0a2d9c048b3a36fbfb
koskim1 <koskim1@naver.com>
2024-05-14 23:38:10 +0900

    OnTriggerEnter 다이얼로그 플레이어 상호작용

--- Assets/_Scripts/DialogueTrigger.cs
+++ Assets/_Scripts/DialogueTrigger.cs
@@ -1,13 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-
+    bool hasPlayer = false;
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    private void Update()
+    {
+        if(hasPlayer && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("hasPlayer = true, TriggerDialogue ½ÇÇà");
+            TriggerDialogue();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hasPlayer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hasPlayer = false;
+        }
     }
 }
