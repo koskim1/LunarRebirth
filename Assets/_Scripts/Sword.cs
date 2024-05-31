0757ebcb38cb96c3b5e31d1030706a747fb0338d
koskim1 <koskim1@naver.com>
2024-05-31 21:44:21 +0900

    무기 에셋 추가

--- Assets/_Scripts/Sword.cs
+++ Assets/_Scripts/Sword.cs
@@ -19,7 +19,7 @@ public class Sword : MonoBehaviour
     {
         // 부모 객체나 필요한 객체에서 AttributesManager를 가져옵니다.
         _attributesManager = GetComponentInParent<AttributesManager>();
-        _playerAnimation = GetComponent<PlayerAnimation>();
+        //_playerAnimation = GetComponent<PlayerAnimation>();
         _boxCollider = GetComponent<BoxCollider>();
     }
 
@@ -28,6 +28,7 @@ public class Sword : MonoBehaviour
         
     }
 
+    // 무기 프리펩 교체시 isTrigger 켜져있는지 항상 확인~~
     private void OnTriggerEnter(Collider other)
     {
         if(_attributesManager == null || !_canDealDamage) return;
@@ -35,13 +36,14 @@ public class Sword : MonoBehaviour
         // 충돌한 객체의 AttributesManager를 가져옵니다.
         var targetAttributesManager = other.GetComponent<AttributesManager>();
         if (targetAttributesManager != null)
-        {          
-            if(_hitTimes.TryGetValue(other.gameObject, out var lastHitTime))
+        {
+            if (_hitTimes.TryGetValue(other.gameObject, out var lastHitTime))
             {
                 // 쿨다운 시간 내에 재충돌하면 무시
                 // 애니메이션이 검을 휘두르고 다시 올라올때 공격이 2번되던 현상 fix
-                if(Time.time - lastHitTime < _hitCooldown)
+                if (Time.time - lastHitTime < _hitCooldown)
                 {
+                    Debug.Log("Cooldown in effect. Skipping damage.");
                     return; // Cooldown 시간 내에 재충돌하면 무시
                 }
             }
