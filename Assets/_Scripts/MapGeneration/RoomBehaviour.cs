ca0f7989c4853e22d24858930c0667084d837fcd
koskim1 <koskim1@naver.com>
2024-07-18 23:55:21 +0900

    절차적 스테이지 생성 구현중 - 2

--- Assets/_Scripts/MapGeneration/RoomBehaviour.cs
+++ Assets/_Scripts/MapGeneration/RoomBehaviour.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class RoomBehaviour : MonoBehaviour
 {
+    public GameObject[] walls; // 0 - Up, 1 - Down, 2 - Right, 3 - Left
+    public GameObject[] doors;
+
+    public bool[] testStatus;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateRoom(testStatus);
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void UpdateRoom(bool[] status)
     {
-        
+        // status배열이 true면 그 방향은 door설정해주고 wall은 없애주기.
+        for(int i = 0; i < status.Length; i++)
+        {
+            doors[i].SetActive(status[i]);
+            walls[i].SetActive(!status[i]);
+        }
     }
+
 }
