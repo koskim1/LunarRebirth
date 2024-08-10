02b1f739302a13270dcc3b6d63ee5a8361f6fa35
koskim1 <koskim1@naver.com>
2024-08-10 16:09:21 +0900

    씬매니저 싱글톤화

--- Assets/_Scripts/0_Title/SceneManagers.cs
+++ Assets/_Scripts/0_Title/SceneManagers.cs
@@ -1,36 +1,79 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneManagers : MonoBehaviour
 {
-    public Animator animator;
+    public static SceneManagers Instance;
+    private Animator animator;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private IEnumerator animatorOnOff()
+    {
+        animator.gameObject.SetActive(false);
+        yield return new WaitForSeconds(0f);
+        animator.gameObject.SetActive(true);        
+    }
 
     public void LoadIntroScene()
     {
+        StartCoroutine(animatorOnOff());        
+        LoadingSceneController.LoadScene("MainStory");
         animator.SetTrigger("FadeOut");
-        LoadingSceneController.LoadScene("MainStory");        
     }
 
     public void LoadMainRoom()
     {
-        animator.SetTrigger("FadeOut");
+        StartCoroutine(animatorOnOff());
         LoadingSceneController.LoadScene("MainRoom");
+        animator.SetTrigger("FadeOut");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if(player != null)
+        {
+            player.transform.position = new Vector3(0, 0, -13f);
+        }
     }
 
     public void LoadDungeon()
     {
-        animator.SetTrigger("FadeOut");
+        StartCoroutine(animatorOnOff());        
         LoadingSceneController.LoadScene("GameScene");
+        animator.SetTrigger("FadeOut");
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            player.transform.position = new Vector3(0, 10f, -10f);
+        }
     }
 
     public void LoadTitleScene()
     {
+        StartCoroutine(animatorOnOff());
         animator.SetTrigger("FadeOut");
         LoadingSceneController.LoadScene("TitleScene");        
     }
 
     public void QuitGame()
     {
+        StartCoroutine(animatorOnOff());
         animator.SetTrigger("FadeOut");
         Application.Quit();
     }
