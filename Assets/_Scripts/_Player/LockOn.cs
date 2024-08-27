8918baad27b9e86ff5e22391a5047ee8cf6f1a57
koskim1 <koskim1@naver.com>
2024-08-27 22:40:49 +0900

    LockOn기능(2) (UI가장 가까운 적에 적용), 카메라 따라가게 추가해야함

--- Assets/_Scripts/_Player/LockOn.cs
+++ Assets/_Scripts/_Player/LockOn.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class LockOn : MonoBehaviour
 {
-    [SerializeField] float lockOnRadius = 6;
+    [SerializeField] float lockOnRadius = 12;
     [SerializeField] LayerMask targetLayer;
-    [SerializeField] float minViewAngle = -70f;
-    [SerializeField] float maxViewAngle = 70f;
+    [SerializeField] float minViewAngle = 0f;
+    [SerializeField] float maxViewAngle = 360f;
 
     [SerializeField] List<EnemyAttributesManager> targetEnemy = new List<EnemyAttributesManager>();
 
+    [SerializeField] Transform lockOnImage;
 
-    private bool isLockOn;
+    EnemyAttributesManager currentTarget;
+    Vector3 currentTargetPosition;
+
+    private bool isLockOn = false;
     private Transform cameraTransform;
     // Start is called before the first frame update
     void Awake()
     {
         cameraTransform = Camera.main.transform;
+    }
+
+    private void Start()
+    {
+        lockOnImage.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isLockOn)
+        {
+            if (isTargetRange())
+            {
+                LookAtTarget();
+            }
+            else
+            {
+                ResetTarget();
+            }
+        }
     }
 
     public void FindLockOnTarget()
@@ -49,18 +73,79 @@ public class LockOn : MonoBehaviour
                         targetEnemy.Add(target);
                     }
                 }
-                // target null일때 없애야하는거 아닌가?
                 else
                 {
                     ResetTarget();
                 }
-
             }
+        }
+
+        LockOnTarget();
+    }
+
+    private void LockOnTarget()
+    {
+        float shortDistance = Mathf.Infinity;
+
+        if(targetEnemy != null)
+        {
+            for (int i = 0; i < targetEnemy.Count; i++)
+            {
+                float distanceFromTarget = Vector3.Distance(transform.position, targetEnemy[i].transform.position);
+
+                if(distanceFromTarget < shortDistance)
+                {
+                    shortDistance = distanceFromTarget;
+                    currentTarget = targetEnemy[i];
+                }
+            }
+        }
+
+        if(currentTarget != null)
+        {
+            FindTarget();
+        }
+
+    }
+
+    private void LookAtTarget()
+    {
+        if(currentTarget == null)
+        {
+            ResetTarget();
+            return;
+        }
+
+        currentTargetPosition = currentTarget.transform.position;
+
+        lockOnImage.position = Camera.main.WorldToScreenPoint(currentTargetPosition);
+    }
+
+    private void FindTarget()
+    {
+        isLockOn = true;
+
+        lockOnImage.gameObject.SetActive(true);
+    }
+
+    private bool isTargetRange()
+    {
+        float distance = (transform.position - currentTargetPosition).magnitude;
+
+        if(distance > lockOnRadius)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
         }
     }
 
     private void ResetTarget()
     {
+        isLockOn = false;
         targetEnemy.Clear();
+        lockOnImage.gameObject.SetActive(false);
     }
 }
