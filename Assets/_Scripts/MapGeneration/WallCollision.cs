a5150ac016b81cbe10393624c615f323e9e13c5a
koskim1 <koskim1@naver.com>
2024-09-06 15:08:34 +0900

    불필요한 던전wall 겹쳐져있는거 최적화

---
+++ Assets/_Scripts/MapGeneration/WallCollision.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.01f);
+
+        foreach(Collider collider in colliders)
+        {
+            if(collider.tag == "Wall")
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+    }
+}
