04fd4c925414ba36aac4d05ce021d791020e80bd
koskim1 <koskim1@naver.com>
2024-10-06 23:40:09 +0900

    보스관련 초반 세팅

---
+++ Assets/_Scripts/Boss/Boss.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    /*
+    
+    HP기반 페이즈 별 패턴변화
+
+    100% ~ 65% = 기본속도 플레이어 추적, 맨손공격
+    
+    30~65% = 소환수 생성 (마법), 속도증가, 공격력 증가
+    
+    0~30% = 부하골렘 소환 ( 안죽이면 보스 hp안닳음 ) , 소환수 더 생성
+    
+    보스 LockOn은 시점이나 테스트 좀 더 해봐야할듯
+    
+    EnemyAttributesManager 붙여주고 , Layer설정 잊지말기
+
+    */
+}
